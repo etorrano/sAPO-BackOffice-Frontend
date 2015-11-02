275c280282e02f425f0311ab56ad0842d3312966
etorrano <emiliano.torrano@fing.edu.uy>
2015-11-02 11:52:41 -0300

    se agrega estructura reportes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,150 +18,150 @@ angular
     'ngTouch',
     'ui.tree',
     'ngTable'
-  ])
-    .value('fbURL', 'https://angular-ui-tree.firebaseio.com/demo/groups/')
-    .factory('Groups', ['$http', '$resource', 'REST_API', function($http, $resource, REST_API) {
-      var API_REST_URL = REST_API.BASE_URL;
-      console.log("En modelo Catlate");
-      return $resource(API_REST_URL + ':recurso/:modulo/:submodulo/:id', {}, {
-        crearCatlate   : { method: 'POST', params: { recurso: 'templates', modulo: 'create'}},
-        getTemplate     : { method: 'GET', params: { recurso: 'templates' ,id: '@id'},  isArray: false },
-        getTemplates    : { method: 'GET', params: { recurso: 'templates' },  isArray: true },
-        actualizarTemplate   : { method: 'PUT', params: { recurso: 'templates',id: '@id'}},
-        eliminarTemplate   : { method: 'DELETE', params: { recurso: 'templates',id: '@id'}}
-      })
-    }])
-    .controller('groupsCtrl', function($scope, $log, Groups,ServicioTemplate) {
-
-      $scope.info = "";
-      $scope.groups = [];
-      ServicioTemplate.getTemplates().then(function(templates) {
-        $scope.groups = templates;
-      });
-      $scope.$watch(function () {
-            return Groups.$getIndex();
-          },
-          function() {
-            $scope.groups = [];/*
-            var index = Groups.$getIndex();
-            if (index.length > 0) {
-              for (var i = 0; i < index.length; i++) {
-                var group = Groups[index[i]];
-                if (group) {
-                  group.id = index[i];
-                  group.editing = false;
-                  if (!group.categories) {
-                    group.categories = [];
-                  }
-                  group.$firebase = $firebase(new Firebase(fbURL + group.id)); // jshint ignore:line
-                  group.destroy = function() {
-                    this.$firebase.$remove();
-                  };
-                  group.save = function() {
-                    this.$firebase.name = this.name;
-                    this.$firebase.sortOrder = this.sortOrder;
-                    this.$firebase.categories = this.categories;
-                    this.$firebase.$save();
-                    this.editing = false;
-                  };
-                  $scope.groups.push(group);
-                }
-              }
-              $scope.groups.sort(function(group1, group2) {
-                return group1.sortOrder - group2.sortOrder;
-              });
-            }*/
-          }, true);
-
-      $scope.addGroup = function() {
-        if ($scope.groups.length > 10) {
-          window.alert('You can\'t add more than 10 groups!');
-          return;
-        }
-        var groupName = document.getElementById("groupName").value;
-        if (groupName.length > 0) {
-          Groups.$add({
-            name: groupName,
-            type: "group",
-            categories: [],
-            sortOrder: $scope.groups.length
-          });
-          document.getElementById("groupName").value = '';
-        }
-      };
-
-      $scope.editGroup = function(group) {
-        group.editing = true;
-      };
-
-      $scope.cancelEditingGroup = function(group) {
-        group.editing = false;
-      };
-
-      $scope.saveGroup = function(group) {
-        group.save();
-      };
-
-      $scope.removeGroup = function(group) {
-        if (window.confirm('Are you sure to remove this group?')) {
-          group.destroy();
-        }
-      };
-
-      $scope.saveGroups = function() {
-        for (var i = $scope.groups.length - 1; i >= 0; i--) {
-          var group = $scope.groups[i];
-          group.sortOrder = i + 1;
-          group.save();
-        }
-      };
-
-      $scope.addCategory = function(group) {
-        if (!group.newCategoryName || group.newCategoryName.length === 0) {
-          return;
-        }
-        group.categories.push({
-          name: group.newCategoryName,
-          sortOrder: group.categories.length,
-          type: "category"
-        });
-        group.newCategoryName = '';
-        group.save();
-      };
-
-      $scope.removeCategory = function(group, category) {
-        if (window.confirm('Are you sure to remove this category?')) {
-          var index = group.categories.indexOf(category);
-          if (index > -1) {
-            group.categories.splice(index, 1)[0];
-          }
-          group.save();
-        }
-      };
-
-      $scope.options = {
-        accept: function(sourceNode, destNodes, destIndex) {
-          var data = sourceNode.$modelValue;
-          var destType = destNodes.$element.attr('data-type');
-          return (data.type == destType); // only accept the same type
-        },
-        dropped: function(event) {
-          console.log(event);
-          var sourceNode = event.source.nodeScope;
-          var destNodes = event.dest.nodesScope;
-          // update changes to server
-          if (destNodes.isParent(sourceNode)
-              && destNodes.$element.attr('data-type') == 'category') { // If it moves in the same group, then only update group
-            var group = destNodes.$nodeScope.$modelValue;
-            group.save();
-          } else { // save all
-            $scope.saveGroups();
-          }
-        }
-      };
-
-
-    })
+     ])
+  //   .value('fbURL', 'https://angular-ui-tree.firebaseio.com/demo/groups/')
+  //   .factory('Groups', ['$http', '$resource', 'REST_API', function($http, $resource, REST_API) {
+  //     var API_REST_URL = REST_API.BASE_URL;
+  //     console.log("En modelo Catlate");
+  //     return $resource(API_REST_URL + ':recurso/:modulo/:submodulo/:id', {}, {
+  //       crearCatlate   : { method: 'POST', params: { recurso: 'templates', modulo: 'create'}},
+  //       getTemplate     : { method: 'GET', params: { recurso: 'templates' ,id: '@id'},  isArray: false },
+  //       getTemplates    : { method: 'GET', params: { recurso: 'templates' },  isArray: true },
+  //       actualizarTemplate   : { method: 'PUT', params: { recurso: 'templates',id: '@id'}},
+  //       eliminarTemplate   : { method: 'DELETE', params: { recurso: 'templates',id: '@id'}}
+  //     })
+  //   }])
+  //   .controller('groupsCtrl', function($scope, $log, Groups,ServicioTemplate) {
+  //
+  //     $scope.info = "";
+  //     $scope.groups = [];
+  //     ServicioTemplate.getTemplates().then(function(templates) {
+  //       $scope.groups = templates;
+  //     });
+  //     $scope.$watch(function () {
+  //           return Groups.$getIndex();
+  //         },
+  //         function() {
+  //           $scope.groups = [];/*
+  //           var index = Groups.$getIndex();
+  //           if (index.length > 0) {
+  //             for (var i = 0; i < index.length; i++) {
+  //               var group = Groups[index[i]];
+  //               if (group) {
+  //                 group.id = index[i];
+  //                 group.editing = false;
+  //                 if (!group.categories) {
+  //                   group.categories = [];
+  //                 }
+  //                 group.$firebase = $firebase(new Firebase(fbURL + group.id)); // jshint ignore:line
+  //                 group.destroy = function() {
+  //                   this.$firebase.$remove();
+  //                 };
+  //                 group.save = function() {
+  //                   this.$firebase.name = this.name;
+  //                   this.$firebase.sortOrder = this.sortOrder;
+  //                   this.$firebase.categories = this.categories;
+  //                   this.$firebase.$save();
+  //                   this.editing = false;
+  //                 };
+  //                 $scope.groups.push(group);
+  //               }
+  //             }
+  //             $scope.groups.sort(function(group1, group2) {
+  //               return group1.sortOrder - group2.sortOrder;
+  //             });
+  //           }*/
+  //         }, true);
+  //
+  //     $scope.addGroup = function() {
+  //       if ($scope.groups.length > 10) {
+  //         window.alert('You can\'t add more than 10 groups!');
+  //         return;
+  //       }
+  //       var groupName = document.getElementById("groupName").value;
+  //       if (groupName.length > 0) {
+  //         Groups.$add({
+  //           name: groupName,
+  //           type: "group",
+  //           categories: [],
+  //           sortOrder: $scope.groups.length
+  //         });
+  //         document.getElementById("groupName").value = '';
+  //       }
+  //     };
+  //
+  //     $scope.editGroup = function(group) {
+  //       group.editing = true;
+  //     };
+  //
+  //     $scope.cancelEditingGroup = function(group) {
+  //       group.editing = false;
+  //     };
+  //
+  //     $scope.saveGroup = function(group) {
+  //       group.save();
+  //     };
+  //
+  //     $scope.removeGroup = function(group) {
+  //       if (window.confirm('Are you sure to remove this group?')) {
+  //         group.destroy();
+  //       }
+  //     };
+  //
+  //     $scope.saveGroups = function() {
+  //       for (var i = $scope.groups.length - 1; i >= 0; i--) {
+  //         var group = $scope.groups[i];
+  //         group.sortOrder = i + 1;
+  //         group.save();
+  //       }
+  //     };
+  //
+  //     $scope.addCategory = function(group) {
+  //       if (!group.newCategoryName || group.newCategoryName.length === 0) {
+  //         return;
+  //       }
+  //       group.categories.push({
+  //         name: group.newCategoryName,
+  //         sortOrder: group.categories.length,
+  //         type: "category"
+  //       });
+  //       group.newCategoryName = '';
+  //       group.save();
+  //     };
+  //
+  //     $scope.removeCategory = function(group, category) {
+  //       if (window.confirm('Are you sure to remove this category?')) {
+  //         var index = group.categories.indexOf(category);
+  //         if (index > -1) {
+  //           group.categories.splice(index, 1)[0];
+  //         }
+  //         group.save();
+  //       }
+  //     };
+  //
+  //     $scope.options = {
+  //       accept: function(sourceNode, destNodes, destIndex) {
+  //         var data = sourceNode.$modelValue;
+  //         var destType = destNodes.$element.attr('data-type');
+  //         return (data.type == destType); // only accept the same type
+  //       },
+  //       dropped: function(event) {
+  //         console.log(event);
+  //         var sourceNode = event.source.nodeScope;
+  //         var destNodes = event.dest.nodesScope;
+  //         // update changes to server
+  //         if (destNodes.isParent(sourceNode)
+  //             && destNodes.$element.attr('data-type') == 'category') { // If it moves in the same group, then only update group
+  //           var group = destNodes.$nodeScope.$modelValue;
+  //           group.save();
+  //         } else { // save all
+  //           $scope.saveGroups();
+  //         }
+  //       }
+  //     };
+  //
+  //
+  //   })
 
   .config(function ($routeProvider) {
     $routeProvider
@@ -171,36 +171,34 @@ angular
         controllerAs: 'prod',
             access: { requiredLogin: true }
       })
-        .when('/templates', {
+
+      .when('/producto/:id', {
+      templateUrl: 'views/producto.html',
+      controller: 'CtrlListarProducto',
+          access: { requiredLogin: true }
+        })
+
+      .when('/productos-listar', {
+        templateUrl: 'views/productos-listar.html',
+        controller: 'CtrlListarProductos',
+        access: { requiredLogin: true }})
+
+      .when('/productos-actualizar/:id', {
+        templateUrl: 'views/productos-actualizar.html',
+        controller: 'CtrlActProd',
+        access: { requiredLogin: true }})
+
+      .when('/productos-crear', {
+        templateUrl: 'views/productos-crear.html',
+        controller: 'CtrlCrearProd',
+        access: { requiredLogin: true }})
+
+      .when('/templates', {
           templateUrl: 'views/templates-listar.html',
           controller: 'CtrlListarTemplates',
             access: { requiredLogin: true }
         })
-        .when('/producto/:id', {
-        templateUrl: 'views/producto.html',
-        controller: 'CtrlListarProducto',
-            access: { requiredLogin: true }
-      })
-      /*  .when('/phones/:phoneId', {
-        templateUrl: 'views/InfoProd.html',
-        controller: 'prodDetCtrl'
-      })*/
-      .when('/productos-listar', {templateUrl: 'views/productos-listar.html', controller: 'CtrlListarProductos',
-            access: { requiredLogin: true }})
 
-      .when('/productos-actualizar/:id', {templateUrl: 'views/productos-actualizar.html', controller: 'CtrlActProd',
-            access: { requiredLogin: true }})
-      .when('/productos-crear', {templateUrl: 'views/productos-crear.html', controller: 'CtrlCrearProd',
-            access: { requiredLogin: true }})
-
-        .when('/demo', {templateUrl: 'views/tree.html', controller: 'groupsCtrl',
-            access: { requiredLogin: true }})
-        .when('/dash', {templateUrl: 'starterbkp.html'})
-/*
-      .when('/producto/:nomprod', {
-        templateUrl: 'templates/user.html',
-        controller: 'UserController'
-      })*/
         .when('/templates-crear', {
           templateUrl : "views/templates-crear.html",
           controller : "CtrlCrearTemp",
@@ -215,22 +213,66 @@ angular
         .when('/templates-actualizar/:id', {templateUrl: 'views/templates-actualizar.html', controller: 'CtrlActTemp',
             access: { requiredLogin: true }})
 
-        .when('/categorias-crear', {
-            templateUrl : "views/categorias-crear.html",
-            controller : "CtrlCrearCat",
-            access: { requiredLogin: true }
-        })
-        .when('/categorias-listar', {
-            templateUrl : "views/categorias-listar.html",
-            controller : "CtrlListarCategorias",
-            access: { requiredLogin: true }
-        })
+      /*  .when('/phones/:phoneId', {
+        templateUrl: 'views/InfoProd.html',
+        controller: 'prodDetCtrl'
+      })*/
 
-        .when('/categorias-actualizar/:id', {
-          templateUrl: 'views/categorias-actualizar.html',
-          controller: 'CtrlActCat',
+      .when('/categorias-crear', {
+          templateUrl : "views/categorias-crear.html",
+          controller : "CtrlCrearCat",
           access: { requiredLogin: true }
+      })
+      .when('/categorias-listar', {
+          templateUrl : "views/categorias-listar.html",
+          controller : "CtrlListarCategorias",
+          access: { requiredLogin: true }
+      })
+
+      .when('/categorias-actualizar/:id', {
+        templateUrl: 'views/categorias-actualizar.html',
+        controller: 'CtrlActCat',
+        access: { requiredLogin: true }
+      })
+
+      .when('/reportes/usuariosreg', {
+        templateUrl: 'views/reportes/reportes-usuariosreg.html',
+        controller: 'CtrlReportesUsuariosReg',
+        access: { requiredLogin: true }
+      })
+
+      .when('/reportes/fraude', {
+        templateUrl: 'views/reportes/reportes-fraude.html',
+        controller: 'CtrlReportesFraude',
+        access: { requiredLogin: true }
+      })
+
+      .when('/reportes/ganancias', {
+        templateUrl: 'views/reportes/reportes-ganancias.html',
+        controller: 'CtrlReportesGanancias',
+        access: { requiredLogin: true }
+      })
+
+      .when('/reportes/prodgenerico', {
+        templateUrl: 'views/reportes/reportes-prodgenerico.html',
+        controller: 'CtrlReportesProdgenerico',
+        access: { requiredLogin: true }
+      })
+
+
+        .when('/demo', {
+          templateUrl: 'views/tree.html',
+          controller: 'groupsCtrl',
+          access: { requiredLogin: true }})
+
+        .when('/dash', {
+          templateUrl: 'starterbkp.html'
         })
+/*
+      .when('/producto/:nomprod', {
+        templateUrl: 'templates/user.html',
+        controller: 'UserController'
+      })*/
 
         .when('/edit', {
           templateUrl : "views/edit.html",
