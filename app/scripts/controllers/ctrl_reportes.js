ee1bc31b690146a1bc36744bf53762f3333c4df7
etorrano <emiliano.torrano@fing.edu.uy>
2015-11-16 08:44:14 -0300

    se mejoran proyecciones

--- app/scripts/controllers/ctrl_reportes.js
+++ app/scripts/controllers/ctrl_reportes.js
@@ -219,11 +219,13 @@ angular.module('sApobackOfficeFrontendApp')
                 },
                 series: [{
                     data: $scope.datos
-                }],
+                }],/*
                 title: {
-                    text: 'Hello'
+                    text: 'Ganancias anuales'
+                },*/
+                title: {
+                    text: 'Ganancias anuales'
                 },
-
                 loading: false
             };
             /* $q.all($scope.chartConfigAreaSplineHistorico.series.data).then(function(){
@@ -249,35 +251,58 @@ angular.module('sApobackOfficeFrontendApp')
                     data: $scope.crecimiento
                 }],
                 title: {
-                    text: 'Hello'
+                    text: 'Crecimiento anual'
                 },
 
                 loading: false
             };
         },this);
+        var proyeccion = 0;
         $scope.proyectar = function () {
             var valor =  $scope.proyeccion;
+            var crecimiento = 0;
+            var proyectado = 0;
             //var cantproyecciones = $scope.cantProyecciones;
             // var arreglo = $scope.datos;//$scope.chartConfigAreaSplineHistorico.series.data;
             console.log("proyeccion " + valor);
             var ultimo = $scope.datos[$scope.datos.length-1];
             var tam =$scope.datos.length;
             //$scope.datos.push($scope.proyeccion);
-            for (var i = 0; i < $scope.cantProyecciones; i++) {
-                sumaDatos+=valor;
-                console.log("valor " + valor);
-                $scope.datos.push(valor);
-                promedios += valor/ultimo;
-                var crecimiento = promedios/(tam);
-                var proyectado = sumaDatos/(tam+1);
-                console.log("proyeccion, sumadatos, tamanio " + valor + sumaDatos + tam);
-                console.log("proyectado " + proyectado);
+            $scope.datos.push(valor);
+            sumaDatos+=valor;
+            console.log("valor " + valor);
+            for (var i = 1; i < $scope.cantProyecciones; i++) {
+                if(proyeccion == 0)
+                {
+                    proyectado = sumaDatos/(tam+1);
+                    crecimiento = proyectado/ultimo;
+                    promedios += crecimiento;
+                    console.log("proyeccion, sumadatos, tamanio " + valor + sumaDatos + tam);
+                    proyeccion = 1;
+                    $scope.datos.push(proyectado);
+                }
+                else
+                {
+                    console.log("promedios,tam-1 " + promedios + tam);
+                    crecimiento = promedios/(tam-1);
+                    proyectado = crecimiento*ultimo;
+                    $scope.datos.push(proyectado);
+                    proyeccion = 0;
+                }
                 $scope.crecimiento.push(crecimiento);
+                console.log("crecimiento " + crecimiento);
+                console.log("proyectado " + proyectado);
                 console.log("ultimo " + ultimo + "promedios " + promedios + "proyectado " + proyectado + "ultimo elem de arreglo " + $scope.datos[$scope.datos.length-1]);
-                angular.copy(ultimo, valor);
+                ultimo = 0;
+                //angular.copy(valor, ultimo);//origen,destino
                 //console.log(myDest);
                 //  angular.extend(ultimo,valor);
-                angular.copy(valor, proyectado);
+              //  console.log("valor " + valor.valueOf);
+                ultimo = proyectado*1;
+                //valor = 0;
+               // valor = proyectado*1;
+                //angular.copy(proyectado, valor);//origen,destino
+               // console.log("ultimo " + ultimo + "valor " + valor);
                 //  angular.extend(valor,proyectado);
                 tam+=1;
             } /*var seriesArray = $scope.chartConfigAreaSplineProyeccion.series;
